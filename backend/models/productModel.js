d31b54396baafeca0d59dc64ff48f31fb5ee6508
Furkan <furkan.gundz00@gmail.com>
2022-10-12 20:22:08 +0300

    Create models.

---
+++ backend/models/productModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    owner_id: {
+        type: String,
+        required: [true, "Please add a owner"],
+    },
+    category: {
+        type: String,
+        required: [true, "Please add a category"],
+    },
+}, { timestamps: true })
+
+
+module.exports = mongoose.model("Product", productSchema)
