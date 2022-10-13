bca77c6ab610eff52fc96b4406e33e9442017abf
Furkan <furkan.gundz00@gmail.com>
2022-10-13 15:02:07 +0300

    Add style home get request response.

--- backend/index.js
+++ backend/index.js
@@ -14,22 +14,22 @@ app.use(cors({ origin: '*' }))
 app.get("/", (req, res) => {
     res.status(200)
         .send(
-            "Succesfully connected. List of endpoints =>\n\
-            \n-------------\
-            \n Post    /user/register            -> kullanıcı adı şifre ve email ile üye kaydetme.\
-            \n POST    /user/login               -> JWT ile beraber giriş yapacak \
+
+            "<h1>Succesfully connected. List of endpoints =></h1>\n\
+            \n<ul><li> Post    /user/register            -> kullanıcı adı şifre ve email ile üye kaydetme.</li>\
+            \n<li>POST    /user/login               -> JWT ile beraber giriş yapacak </li>\
             \n------------- \
-            \n POST    /categories               -> Kullanıcı kategori eklemesi yapabilecek \
-            \n GET     /categories               -> Eklediği kategorileri listeleyebilecek\
-            \n GET     /categories/:categoryId   -> Eklediği kategoriyi idsine göre listeleyebilecek\
-            \n PATCH   /categories/:categoryId   -> Eklediği kategoriyi idsine göre güncelleyebilecek\
-            \n DELETE  /categories/:categoryId   -> Eklediği kategoriyi idsine göre silebilecek (Eğer bir kategori silinirse, o kategoriye ait -varsa- ürünler silinecek)\
+            \n<li> POST    /categories               -> Kullanıcı kategori eklemesi yapabilecek </li>\
+            \n<li>GET     /categories               -> Eklediği kategorileri listeleyebilecek</li>\
+            \n<li>GET     /categories/:categoryId   -> Eklediği kategoriyi idsine göre listeleyebilecek</li>\
+            \n<li> PATCH   /categories/:categoryId   -> Eklediği kategoriyi idsine göre güncelleyebilecek</li>\
+            \n<li>DELETE  /categories/:categoryId   -> Eklediği kategoriyi idsine göre silebilecek (Eğer bir kategori silinirse, o kategoriye ait -varsa- ürünler silinecek)</li>\
             \n------------- \
-            \n POST    /products                 -> Bir kategoriye, yeni ürün kaydı oluşturabilecek\
-            \n GET     /products                 -> Kullanıcı eklediği ürünler listeleyebilecek\
-            \n GET     /products/:productId      -> Kullanıcı eklediği ürünü idsine göre listeleyebilecek\
-            \n PATCH   /products/:productId      -> Kullanıcı eklediği ürünü idsine göre güncelleyebilecek\
-            \n DELETE  /products/:productId      -> Kullanıcı eklediği ürünü idsine göre silebilecek")
+            \n<li>POST    /products                 -> Bir kategoriye, yeni ürün kaydı oluşturabilecek</li>\
+            \n<li> GET     /products                 -> Kullanıcı eklediği ürünler listeleyebilecek</li>\
+            \n<li>GET     /products/:productId      -> Kullanıcı eklediği ürünü idsine göre listeleyebilecek</li>\
+            \n<li>PATCH   /products/:productId      -> Kullanıcı eklediği ürünü idsine göre güncelleyebilecek</li>\
+            \n<li> DELETE  /products/:productId      -> Kullanıcı eklediği ürünü idsine göre silebilecek</li></ul>")
 })
 
 app.use("/user", require("./routes/userRoutes"))
